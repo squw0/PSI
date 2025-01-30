9249ed245b64caa4d5f2c098a1fd2f28002220ab
Piotr Zawadzki <115027061+squw0@users.noreply.github.com>
2025-01-30 23:12:26 +0100

    fixed lab6

---
+++ lab6/zad2.py
@@ -0,0 +1,66 @@
+from skfuzzy import control as ctrl
+import skfuzzy as fuzz
+import numpy as np
+
+class washing_machine:
+    degree_dirt = ctrl.Antecedent(np.arange(0, 101, 1), 'degree_dirt')
+    type_dirt = ctrl.Antecedent(np.arange(0, 101, 1), 'type_dirt')
+    wash_time = ctrl.Consequent(np.arange(0, 61, 1), 'wash_time')
+
+    degree_names = ['Low', 'Medium', 'High']
+    type_names = ['NonFat', 'Medium', 'Fat']
+
+    # Outputing them into auto-membership functions
+    degree_dirt.automf(names=degree_names)
+    type_dirt.automf(names=type_names)
+
+    # Washing Time Universe
+    wash_time['very_short'] = fuzz.trimf(wash_time.universe, [0, 5, 10])
+    wash_time['short'] = fuzz.trimf(wash_time.universe, [5, 10, 20])
+    wash_time['medium'] = fuzz.trimf(wash_time.universe, [15, 25, 35])
+    wash_time['long'] = fuzz.trimf(wash_time.universe, [30, 45, 60])
+    wash_time['VeryLong'] = fuzz.trimf(wash_time.universe, [50, 60, 60])
+
+    # Rule Application
+    rule1 = ctrl.Rule(degree_dirt['High'] | type_dirt['Fat'], wash_time['VeryLong'])
+    rule2 = ctrl.Rule(degree_dirt['Medium'] | type_dirt['Fat'], wash_time['long'])
+    rule3 = ctrl.Rule(degree_dirt['Low'] | type_dirt['Fat'], wash_time['long'])
+    rule4 = ctrl.Rule(degree_dirt['High'] | type_dirt['Medium'], wash_time['long'])
+    rule5 = ctrl.Rule(degree_dirt['Medium'] | type_dirt['Medium'], wash_time['medium'])
+    rule6 = ctrl.Rule(degree_dirt['Low'] | type_dirt['Medium'], wash_time['medium'])
+    rule7 = ctrl.Rule(degree_dirt['High'] | type_dirt['NonFat'], wash_time['medium'])
+    rule8 = ctrl.Rule(degree_dirt['Medium'] | type_dirt['NonFat'], wash_time['short'])
+    rule9 = ctrl.Rule(degree_dirt['Low'] | type_dirt['NonFat'], wash_time['very_short'])
+    rule10 = ctrl.Rule(degree_dirt['High'] & wash_time['short'], wash_time['long'])
+    rule11 = ctrl.Rule(degree_dirt['Low'] & wash_time['long'], wash_time['short'])
+    rule12 = ctrl.Rule(degree_dirt['Medium'] & type_dirt['Fat'], wash_time['long'])
+
+    # Washing Control Simulation
+    washing_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12])
+    washing = ctrl.ControlSystemSimulation(washing_ctrl)
+
+def fuzzify_laundry(fuzz_type, fuzz_degree):
+    washing_machine.washing.input['type_dirt'] = fuzz_type
+    washing_machine.washing.input['degree_dirt'] = fuzz_degree
+
+    washing_machine.washing.compute()
+
+    washing_machine.wash_time.view(sim=washing_machine.washing)
+
+    return washing_machine.washing.output['wash_time']
+ 
+def compute_washing_parameters(type_of_dirt, degree_of_dirt):
+    if type_of_dirt < 0.0 or type_of_dirt > 100.0:
+        raise Exception("Invalid Type of Dirtiness: %lf" % type_of_dirt)
+    if degree_of_dirt < 0.0 or degree_of_dirt > 100.0:
+        raise Exception("Invalid Degree of Dirtiness: %lf" % degree_of_dirt)
+
+    type_fuzzy = fuzzify_laundry(type_of_dirt, degree_of_dirt)
+
+    return type_fuzzy
+
+if __name__ == "__main__":
+    type_of_dirt = float(input("Enter Type of Dirtiness [0-100]: "))
+    degree_of_dirt = float(input("Enter Degree of Dirtiness [0-100]: "))
+    washing_parameters = compute_washing_parameters(type_of_dirt, degree_of_dirt)
+    print("Recommended washing time:", washing_parameters)
---
+++ lab6/zad3.py
@@ -0,0 +1,63 @@
+from skfuzzy import control as ctrl
+import skfuzzy as fuzz
+import numpy as np
+
+class washing_machine:
+    degree_dirt = ctrl.Antecedent(np.arange(0, 101, 1), 'degree_dirt')
+    type_dirt = ctrl.Antecedent(np.arange(0, 101, 1), 'type_dirt')
+    wash_time = ctrl.Consequent(np.arange(0, 61, 1), 'wash_time')
+
+    degree_names = ['Low', 'Medium', 'High']
+    type_names = ['NonFat', 'Medium', 'Fat']
+
+    # Outputing them into auto-membership functions
+    degree_dirt.automf(names=degree_names)
+    type_dirt.automf(names=type_names)
+
+    # Washing Time Universe
+    wash_time['very_short'] = fuzz.trapmf(wash_time.universe, [0, 0, 5, 10])
+    wash_time['short'] = fuzz.trapmf(wash_time.universe, [5, 10, 15, 20])
+    wash_time['medium'] = fuzz.trapmf(wash_time.universe, [15, 25, 35, 40])
+    wash_time['long'] = fuzz.trapmf(wash_time.universe, [30, 40, 50, 60])
+    wash_time['VeryLong'] = fuzz.trapmf(wash_time.universe, [50, 60, 60, 60])
+
+    # Rule Application
+    rule1 = ctrl.Rule(degree_dirt['High'] | type_dirt['Fat'], wash_time['VeryLong'])
+    rule2 = ctrl.Rule(degree_dirt['Medium'] | type_dirt['Fat'], wash_time['long'])
+    rule3 = ctrl.Rule(degree_dirt['Low'] | type_dirt['Fat'], wash_time['long'])
+    rule4 = ctrl.Rule(degree_dirt['High'] | type_dirt['Medium'], wash_time['long'])
+    rule5 = ctrl.Rule(degree_dirt['Medium'] | type_dirt['Medium'], wash_time['medium'])
+    rule6 = ctrl.Rule(degree_dirt['Low'] | type_dirt['Medium'], wash_time['medium'])
+    rule7 = ctrl.Rule(degree_dirt['High'] | type_dirt['NonFat'], wash_time['medium'])
+    rule8 = ctrl.Rule(degree_dirt['Medium'] | type_dirt['NonFat'], wash_time['short'])
+    rule9 = ctrl.Rule(degree_dirt['Low'] | type_dirt['NonFat'], wash_time['very_short'])
+
+    # Washing Control Simulation
+    washing_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9])
+    washing = ctrl.ControlSystemSimulation(washing_ctrl)
+
+def fuzzify_laundry(fuzz_type, fuzz_degree):
+    washing_machine.washing.input['type_dirt'] = fuzz_type
+    washing_machine.washing.input['degree_dirt'] = fuzz_degree
+
+    washing_machine.washing.compute()
+
+    washing_machine.wash_time.view(sim=washing_machine.washing)
+ 
+    return washing_machine.washing.output['wash_time']
+
+def compute_washing_parameters(type_of_dirt, degree_of_dirt):
+    if type_of_dirt < 0.0 or type_of_dirt > 100.0:
+        raise Exception("Invalid Type of Dirtiness: %lf" % type_of_dirt)
+    if degree_of_dirt < 0.0 or degree_of_dirt > 100.0:
+        raise Exception("Invalid Degree of Dirtiness: %lf" % degree_of_dirt)
+
+    type_fuzzy = fuzzify_laundry(type_of_dirt, degree_of_dirt)
+
+    return type_fuzzy
+
+if __name__ == "__main__":
+    type_of_dirt = float(input("Enter Type of Dirtiness [0-100]: "))
+    degree_of_dirt = float(input("Enter Degree of Dirtiness [0-100]: "))
+    washing_parameters = compute_washing_parameters(type_of_dirt, degree_of_dirt)
+    print("Recommended washing time:", washing_parameters)
